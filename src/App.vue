51dcdf5b53afa4b42cb7ef390cb0bb1f5ed8f8b2
Adrien Maranville <Adrien@Maranville.IO>
2018-08-18 13:48:50 -0600

    added ability to upload a file

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,69 @@
 <template>
   <div id="app">
-    Let's build an Instagram Clone!
+    <div class="app-phone">
+      <div class="phone-header">
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1211695/vue_gram_logo_cp.png" />
+      </div>
+      <phone-body :step="step"
+                  :posts="posts"
+                  :filters="filters"
+                  :image="image"
+                  :selectedFilter="selectedFilter"
+                  v-model="caption"
+      />
+      <div class="phone-footer">
+        <div class="home-cta">
+          <i class="fas fa-home fa-lg"></i>
+        </div>
+        <div class="upload-cta">
+          <input type="file"
+                 name="file"
+                 id="file"
+                 class="inputfile"
+                 @change="uploadImage"/>
+          <label for="file">
+            <i class="far fa-plus-square fa-lg"></i>
+          </label>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+  import PhoneBody from "./components/PhoneBody";
+  import posts from "../data/posts";
+  import filters from "../data/filters";
+
   export default {
-    name: "App"
+    name: "App",
+    data() {
+      return {
+        step: 1,
+        posts,
+        filters,
+        image: '',
+        selectedFilter: '',
+        caption: 'caption',
+      };
+    },
+    methods: {
+      uploadImage(evt) {
+        const files = evt.target.files;
+        if (!files.length) return;
+
+        const reader = new FileReader();
+        reader.readAsDataURL(files[0]);
+        reader.onload = evt => {
+          this.image = evt.target.result;
+          this.step = 2;
+        };
+        document.querySelector("#file").value = "";
+      }
+    },
+    components: {
+      "phone-body": PhoneBody
+    }
   };
 </script>
 
---
+++ src/components/PhoneBody.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="phone-body">
+    <div v-if="step === 1" class="feed">
+      <vuegram-post v-for="post in posts"
+                    :post="post"
+                    :key="posts.indexOf(post)">
+      </vuegram-post>
+    </div>
+    <div v-if="step === 2">
+      <div class="selected-image"
+           :style="{ backgroundImage: 'url(' + image + ')' }"></div>
+      <div class="filter-container">
+        <!-- Where filter choices will be -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import VuegramPost from "./VuegramPost";
+
+  export default {
+    name: "PhoneBody",
+    props: {
+      step: Number,
+      posts: Array,
+      filters: Array,
+      image: String
+    },
+    components: {
+      "vuegram-post": VuegramPost
+    }
+  };
+</script>
+
+<style lang="scss" src="../../styles/phone-body.scss">
+  // Styles from stylesheet
+</style>
